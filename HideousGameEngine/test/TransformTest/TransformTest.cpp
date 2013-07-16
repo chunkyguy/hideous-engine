165b652c1a54245edcbaf51d055e6cc699ed6f36
Sidharth Juyal <sid@whackylabs.com>
2013-07-16 13:09:31 +0530

    Begin work on he::View heirarchy

---
+++ HideousGameEngine/test/TransformTest/TransformTest.cpp
@@ -0,0 +1,9 @@
+//
+//  TransformTest.cpp
+//  HideousGameEngine
+//
+//  Created by Sid on 14/07/13.
+//  Copyright (c) 2013 whackylabs. All rights reserved.
+//
+
+#include "TransformTest.h"
---
+++ HideousGameEngine/test/TransformTest/TransformTest.h
@@ -0,0 +1,14 @@
+//
+//  TransformTest.h
+//  HideousGameEngine
+//
+//  Created by Sid on 14/07/13.
+//  Copyright (c) 2013 whackylabs. All rights reserved.
+//
+
+#ifndef __HideousGameEngine__TransformTest__
+#define __HideousGameEngine__TransformTest__
+
+#include <iostream>
+
+#endif /* defined(__HideousGameEngine__TransformTest__) */
