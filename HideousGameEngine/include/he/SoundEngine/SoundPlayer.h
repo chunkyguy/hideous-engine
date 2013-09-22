bfe4d2c126f415ca50e1042d4ea6283a473e1c88
Sidharth Juyal <sid@whackylabs.com>
2013-09-22 15:58:47 +0530

    Sound Engine: missing files

---
+++ HideousGameEngine/include/he/SoundEngine/SoundPlayer.h
@@ -0,0 +1,43 @@
+//
+//  SoundPlayer.h
+//  HideousGameEngine
+//
+//  Created by Sid on 22/09/13.
+//  Copyright (c) 2013 whackylabs. All rights reserved.
+//
+
+#ifndef HideousGameEngine_SoundPlayer_h
+#define HideousGameEngine_SoundPlayer_h
+#include <string>
+
+#define MAX_EFFECTS 10
+
+namespace he{
+	
+	class Sound {
+	public:
+		Sound(const char *filename, const char *extn, int loop = -1);
+		~Sound();
+
+		void *player_;	// AVAudioPlayer
+		std::string name_;
+	};
+	
+	class SoundPlayer {
+	public:
+		SoundPlayer();
+		~SoundPlayer();
+		
+		Sound *music;
+		Sound *effects[MAX_EFFECTS];
+		int effects_counter;
+	};
+	
+	void SoundPlayer_PlayMusic(SoundPlayer *player, const char *filename, const char *extn);
+	void SoundPlayer_PlayEffect(SoundPlayer *player, const char *filename, const char *extn);
+	void SoundPlayer_StopAllEffects(SoundPlayer *player);
+}
+
+extern he::SoundPlayer *g_SoundPlayer;
+
+#endif
