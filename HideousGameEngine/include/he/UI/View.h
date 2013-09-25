10ad3393f03b5f87c265c05548a7b6e0ff058da3
Sidharth Juyal <sid@whackylabs.com>
2013-09-25 08:01:26 +0530

    View now comes with default size

--- HideousGameEngine/include/he/UI/View.h
+++ HideousGameEngine/include/he/UI/View.h
@@ -11,69 +11,70 @@
 #include <he/Utils/Frame.h>
 
 namespace he {
-		/** Container of all kind of Views.
-		 Adds child component in stack order.
-		 If AddSubview-order is like [btn_01, btn_01, bg, icon_01]
-		 Draw order will be [btn_01, btn_01, bg, icon_01]
-		 */
-		class View{
-		public:
-			View(const Transform &transform);
-			virtual ~View();
-			
-			/** Update self. Update all children in draw order */
-			virtual void Update(float dt);
-						
-			/** Render self. Render all children in draw order */
-			virtual void Render();
-			
-			virtual GLKVector2 GetSize() const;
-			
-			/**
-			 *	Owns the passed component
-			 *
-			 *	@param	view	 The view to be moved. Is owned.
-			 *
-			 *	@return	Reference to the view.
-			 *
-			 *	@note	Use like: View *my_view = some_view->MoveSubview(new View(...));
-			 *			The point is to keep new inside the MoveSubview all the time. It's just a matter of convention.
-			 */
-			View *MoveSubview(View *view);
-
-			/** Remove the passed component */
-			void RemoveSubview(View *view);
-
-			/** Update frame */
-			void SetTransform(const Transform &transform);
-			/** Get the current frame */
-			const Transform &GetTransform() const;
-			
-//			/** Returns pointer to transform
-//			@warning This is more like a design bug, can get modified in future. Use minimum.
-//			 */
-//			Transform *GetTransformPtr();
-
-		protected:
-			Transform transform_; /**< The frame of the view */
-
-		private:
-			View *head_;		/**< The head pointer to the first child UIComponent. Starts drawing from here */
-			View *tail_;	/**< Next Submit adds to this node. */
-			View *next_;	/**< Points to the next sibling UIComponent */
-	};
-
-	Frame CreateFrame(const View *view);
-	
-	/**
-	 *	Collision detection test between two View objects.
-	 *
-	 *	@param	one	View one.
-	 *	@param	two	View two.
-	 *
-	 *	@return	true if the collision happens else false.
-	 */
-	bool HitTest(const he::View *one, const he::View *two);
+ /** Container of all kind of Views.
+  Adds child component in stack order.
+  If AddSubview-order is like [btn_01, btn_01, bg, icon_01]
+  Draw order will be [btn_01, btn_01, bg, icon_01]
+  */
+ class View{
+ public:
+  View(const Transform &transform, const GLKVector2 &size = GLKVector2Make(0.0f, 0.0f));
+  virtual ~View();
+  
+  /** Update self. Update all children in draw order */
+  virtual void Update(float dt);
+  
+  /** Render self. Render all children in draw order */
+  virtual void Render();
+  
+  virtual GLKVector2 GetSize() const;
+  
+  /**
+   *	Owns the passed component
+   *
+   *	@param	view	 The view to be moved. Is owned.
+   *
+   *	@return	Reference to the view.
+   *
+   *	@note	Use like: View *my_view = some_view->MoveSubview(new View(...));
+   *			The point is to keep new inside the MoveSubview all the time. It's just a matter of convention.
+   */
+  View *MoveSubview(View *view);
+  
+  /** Remove the passed component */
+  void RemoveSubview(View *view);
+  
+  /** Update frame */
+  void SetTransform(const Transform &transform);
+  /** Get the current frame */
+  const Transform &GetTransform() const;
+  
+  //			/** Returns pointer to transform
+  //			@warning This is more like a design bug, can get modified in future. Use minimum.
+  //			 */
+  //			Transform *GetTransformPtr();
+  
+ protected:
+  Transform transform_; /**< The frame of the view */
+  
+ private:
+  View *head_;		/**< The head pointer to the first child UIComponent. Starts drawing from here */
+  View *tail_;	/**< Next Submit adds to this node. */
+  View *next_;	/**< Points to the next sibling UIComponent */
+  GLKVector2 default_size_; /**< Default size */
+ };
+ 
+ Frame CreateFrame(const View *view);
+ 
+ /**
+  *	Collision detection test between two View objects.
+  *
+  *	@param	one	View one.
+  *	@param	two	View two.
+  *
+  *	@return	true if the collision happens else false.
+  */
+ bool HitTest(const he::View *one, const he::View *two);
 } /*namespace he*/
 
 #endif
--- HideousGameEngine/src/UI/View.cpp
+++ HideousGameEngine/src/UI/View.cpp
@@ -10,156 +10,157 @@
 #include <he/Utils/Utils.h>
 
 namespace he {
-	View::View(const Transform &transform) :
-	head_(nullptr),
-	tail_(nullptr),
-	next_(nullptr),
-	transform_(transform)
-	{}
-	
-	View::~View(){
-		if(head_){
-			View *del = nullptr;
-			for(View *p = head_; p; p = p->next_){
-				if(del){
-					delete del; del = nullptr;
-				}
-				del = p;
-			}
-			if(del){
-				delete del; del = nullptr;
-			}
-		}
-	}
-	
-	void View::Update(float dt){
-		for(View *p = head_; p; p = p->next_){
-			p->Update(dt);
-		}
-	}
-	
-	void View::Render(){
-		for(View *p = head_; p; p = p->next_){
-			p->Render();
-		}
-	}
-	
-	GLKVector2 View::GetSize() const {
-		return GLKVector2Make(0.0f, 0.0f);
-	}
-	
-	/** Owns the passed component */
-	View *View::MoveSubview(View *view){
-		if(!head_){
-			head_ = tail_ = view;
-		}else{
-			tail_->next_ = view;
-			tail_ = view;
-		}
-		view->transform_.parent = &transform_;
-		return view;
-	}
-	
-	void View::RemoveSubview(View *view){
-		if(view == head_){
-			head_ = head_->next_;
-			if (view == tail_) {		// if only one node exists. Adjust the tail_ too.
-				tail_ = head_;
-			}
-		}else{
-			View *p = nullptr;
-			for(p = head_; p->next_ != view; p = p->next_){
-			}
-			p->next_ = view->next_;
-			if (view == tail_) {	// if deleting the last node. Move tail_ one step back.
-				tail_ = p;
-			}
-		}
-		delete view;
-		view = nullptr;
-	}
-	
-	void View::SetTransform(const he::Transform &transform) {
-		transform_ = transform;
-	}
-	
-	const Transform &View::GetTransform() const{
-		return transform_;
-	}
-	
-	Frame CreateFrame(const View *view) {
-		return Frame(view->GetTransform(), view->GetSize());
-	}
-	
-	//	Frame *View::GetFramePtr(){
-	//		return &frame_;
-	//	}
-	
-	//		const GLKMatrix4 View::GetMVP() const{
-	//			return frame_.GetTransform().GetMVP();
-	//		}
-	//
-	//		bool View::Contains(const GLKVector2 &point){
-	//			return he::Vertex::Contains(frame_.GetRect(), point);
-	//		}
-	
-	
-	
-	bool HitTest(const he::View *one, const he::View *two) {
-		struct Line {
-			int a, b;
-		} lines[4] = {
-			{0,1},		//AB
-			{0,2},		//AC
-			{1,3},		//BD
-			{2,3}		//CD
-		};
-		
-		GLKVector2 size = one->GetSize();
-		GLKVector2 v_one_i[4] = {
-			{-size.x/2.0f, -size.y/2.0f},
-			{size.x/2.0f, -size.y/2.0f},
-			{-size.x/2.0f, size.y/2.0f},
-			{size.x/2.0f, size.y/2.0f}
-		};
-		Transform_GetWorldCoordinates(one->GetTransform(), 4, v_one_i);
-		
-		size = two->GetSize();
-		GLKVector2 v_two_i[4] = {
-			{-size.x/2.0f, -size.y/2.0f},
-			{size.x/2.0f, -size.y/2.0f},
-			{-size.x/2.0f, size.y/2.0f},
-			{size.x/2.0f, size.y/2.0f}
-		};
-		Transform_GetWorldCoordinates(two->GetTransform(), 4, v_two_i);
-		
-		
-		int l_one, l_two;
-		bool hit = false;
-		for (l_one = 0; l_one < 4; ++l_one) {
-			for (l_two = 0; l_two < 4; ++l_two) {
-				if (LinesIntersect(v_one_i[lines[l_one].a], v_one_i[lines[l_one].b], v_two_i[lines[l_two].a], v_two_i[lines[l_two].b])) {
-					hit = true;
-					//he_Trace("Line1[%d]: {%@, %@}\nLine2[%d]: {%@, %@}\n",l_one,v_one_i[lines[l_one].a], v_one_i[lines[l_one].b],l_two, v_two_i[lines[l_two].a], v_two_i[lines[l_two].b]);
-				}
-				if (hit) {break;}
-			}
-			if (hit) {break;}
-		}
-		
-//		he_Trace("Trans[0]:%@\nSize[0]:%@\n",one->GetTransform(), one->GetSize());
-//		he_Trace("Trans[1]:%@\nSize[1]:%@\n",two->GetTransform(), two->GetSize());
-		//			he_Trace("View[1]:\n%@\n",two_mv);
-		//			for (int i = 0; i < 4; ++i) {
-		//				he_Trace("one: %d:\t%@\n",i,v_one_i[i]);
-		//			}
-		//			for (int i = 0; i < 4; ++i) {
-		//				he_Trace("two: %d:\t%@\n",i,v_two_i[i]);
-		//			}
-			
-		return hit;
-	}
-	
+ View::View(const Transform &transform, const GLKVector2 &size) :
+ head_(nullptr),
+ tail_(nullptr),
+ next_(nullptr),
+ transform_(transform),
+ default_size_(size)
+ {}
+ 
+ View::~View(){
+  if(head_){
+   View *del = nullptr;
+   for(View *p = head_; p; p = p->next_){
+    if(del){
+     delete del; del = nullptr;
+    }
+    del = p;
+   }
+   if(del){
+    delete del; del = nullptr;
+   }
+  }
+ }
+ 
+ void View::Update(float dt){
+  for(View *p = head_; p; p = p->next_){
+   p->Update(dt);
+  }
+ }
+ 
+ void View::Render(){
+  for(View *p = head_; p; p = p->next_){
+   p->Render();
+  }
+ }
+ 
+ GLKVector2 View::GetSize() const {
+  return default_size_;
+ }
+ 
+ /** Owns the passed component */
+ View *View::MoveSubview(View *view){
+  if(!head_){
+   head_ = tail_ = view;
+  }else{
+   tail_->next_ = view;
+   tail_ = view;
+  }
+  view->transform_.parent = &transform_;
+  return view;
+ }
+ 
+ void View::RemoveSubview(View *view){
+  if(view == head_){
+   head_ = head_->next_;
+   if (view == tail_) {		// if only one node exists. Adjust the tail_ too.
+    tail_ = head_;
+   }
+  }else{
+   View *p = nullptr;
+   for(p = head_; p->next_ != view; p = p->next_){
+   }
+   p->next_ = view->next_;
+   if (view == tail_) {	// if deleting the last node. Move tail_ one step back.
+    tail_ = p;
+   }
+  }
+  delete view;
+  view = nullptr;
+ }
+ 
+ void View::SetTransform(const he::Transform &transform) {
+  transform_ = transform;
+ }
+ 
+ const Transform &View::GetTransform() const{
+  return transform_;
+ }
+ 
+ Frame CreateFrame(const View *view) {
+  return Frame(view->GetTransform(), view->GetSize());
+ }
+ 
+ //	Frame *View::GetFramePtr(){
+ //		return &frame_;
+ //	}
+ 
+ //		const GLKMatrix4 View::GetMVP() const{
+ //			return frame_.GetTransform().GetMVP();
+ //		}
+ //
+ //		bool View::Contains(const GLKVector2 &point){
+ //			return he::Vertex::Contains(frame_.GetRect(), point);
+ //		}
+ 
+ 
+ 
+ bool HitTest(const he::View *one, const he::View *two) {
+  struct Line {
+   int a, b;
+  } lines[4] = {
+   {0,1},		//AB
+   {0,2},		//AC
+   {1,3},		//BD
+   {2,3}		//CD
+  };
+  
+  GLKVector2 size = one->GetSize();
+  GLKVector2 v_one_i[4] = {
+   {-size.x/2.0f, -size.y/2.0f},
+   {size.x/2.0f, -size.y/2.0f},
+   {-size.x/2.0f, size.y/2.0f},
+   {size.x/2.0f, size.y/2.0f}
+  };
+  Transform_GetWorldCoordinates(one->GetTransform(), 4, v_one_i);
+  
+  size = two->GetSize();
+  GLKVector2 v_two_i[4] = {
+   {-size.x/2.0f, -size.y/2.0f},
+   {size.x/2.0f, -size.y/2.0f},
+   {-size.x/2.0f, size.y/2.0f},
+   {size.x/2.0f, size.y/2.0f}
+  };
+  Transform_GetWorldCoordinates(two->GetTransform(), 4, v_two_i);
+  
+  
+  int l_one, l_two;
+  bool hit = false;
+  for (l_one = 0; l_one < 4; ++l_one) {
+   for (l_two = 0; l_two < 4; ++l_two) {
+    if (LinesIntersect(v_one_i[lines[l_one].a], v_one_i[lines[l_one].b], v_two_i[lines[l_two].a], v_two_i[lines[l_two].b])) {
+     hit = true;
+     //he_Trace("Line1[%d]: {%@, %@}\nLine2[%d]: {%@, %@}\n",l_one,v_one_i[lines[l_one].a], v_one_i[lines[l_one].b],l_two, v_two_i[lines[l_two].a], v_two_i[lines[l_two].b]);
+    }
+    if (hit) {break;}
+   }
+   if (hit) {break;}
+  }
+  
+  //		he_Trace("Trans[0]:%@\nSize[0]:%@\n",one->GetTransform(), one->GetSize());
+  //		he_Trace("Trans[1]:%@\nSize[1]:%@\n",two->GetTransform(), two->GetSize());
+  //			he_Trace("View[1]:\n%@\n",two_mv);
+  //			for (int i = 0; i < 4; ++i) {
+  //				he_Trace("one: %d:\t%@\n",i,v_one_i[i]);
+  //			}
+  //			for (int i = 0; i < 4; ++i) {
+  //				he_Trace("two: %d:\t%@\n",i,v_two_i[i]);
+  //			}
+  
+  return hit;
+ }
+ 
 } /*namespace he*/
 
 ///EOF
