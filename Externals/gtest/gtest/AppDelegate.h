83bc03687a8d7367f6dc5dfef98678c501bd66f7
Sidharth Juyal <sid@whackylabs.com>
2014-10-14 15:23:14 +0530

    Add missing files

---
+++ Externals/gtest/gtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gtest
+//
+//  Created by Sid on 15/06/13.
+//  Copyright (c) 2013 whackylabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Externals/gtest/gtest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gtest
+//
+//  Created by Sid on 15/06/13.
+//  Copyright (c) 2013 whackylabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
